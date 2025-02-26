d98652f7e037dd8e6faebc186eec71a7d46146bf
Divyapriya-K <diyapriya0428@gmail.com>
2025-02-26 11:19:29 +0530

    Completed

--- tailwind.js
+++ tailwind.js
@@ -1,12 +1,16 @@
-// tailwind.config.js
-module.exports = {
-    theme: {
-      extend: {
-        spacing: {
-          'custom-space': '1.25rem', // Example
-          'larger': '2rem',
-        }
-      }
-    },
-    plugins: [],
-  }
+// // tailwind.config.js
+// module.exports = {
+//     theme: {
+//       extend: {
+//         spacing: {
+//           'custom-space': '1.25rem', // Example
+//           'larger': '2rem',
+//         }
+//       }
+//     },
+//     plugins: [],
+//   }
+
+
+  
+  
